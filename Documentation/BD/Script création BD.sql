19815dcaafb4e18c529bc64ffee159836f418af7
Bastien PERRUFFEL <126145356+BastienPRFL@users.noreply.github.com>
2025-09-15 11:43:49 +0200

    Add files via upload

---
+++ Documentation/BD/Script création BD.sql
@@ -0,0 +1,20 @@
+CREATE TABLE Compte(
+	login VARCHAR(20),
+	mdp VARCHAR(50),
+	statut CHAR,
+	mail VARCHAR(100),
+	CONSTRAINT pk_Compte PRIMARY KEY (login)
+	CONSTRAINT ck_mail_compte CHECK (mail REGEXP '^.+@.+\..+$')
+	);
+	
+CREATE TABLE Membre(
+	idMembre VARCHAR(20),
+	nom VARCHAR(50),
+	prenom VARCHAR(50),
+	telephone VARCHAR(30),
+	mail VARCHAR(100),
+	CONSTRAINT pk_Membre PRIMARY KEY (idMembre),
+	CONSTRAINT ck_mail_membre CHECK (mail REGEXP '.+@.+\..+'),
+	CONSTRAINT ck_telephone_membre CHECK (telephone REGEXP '^\+[0-9 ]+$')
+	);
+	
