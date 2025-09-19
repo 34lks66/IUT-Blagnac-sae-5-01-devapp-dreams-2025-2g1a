dd244c1879fb2aa6c5a63af3ea1c150b9b3d1ea0
sidourhs <sidneyrichards31@gmail.com>
2025-09-19 16:55:08 +0200

    ajout survol du texte couleur...

--- dev/site_dreams/src/components/NavBar.tsx
+++ dev/site_dreams/src/components/NavBar.tsx
@@ -14,21 +14,34 @@ const itemColors = [
   "border-blue-500",
 ];
 
+const textHoverColors = [
+  "hover:text-red-500",
+  "hover:text-orange-500",
+  "hover:text-yellow-400",
+  "hover:text-green-300",
+  "hover:text-blue-500",
+];
+
 const NavBar = () => {
   return (
     <nav className="p-3">
-      <ul className="flex flex-row gap-14 m-0 list-none items-center">
-        {navItems.map((item, idx) => (
-          <li
-            key={item.href}
-            className={`px-0 py-1 border-t-2 border-b-2 ${itemColors[idx]}`}
-          >
-            <a href={item.href} className="block text-left font-medium">
-              {item.name}
-            </a>
-          </li>
-        ))}
-      </ul>
+      <div className="w-full md:w-3/4 md:ml-auto">
+        <ul className="flex flex-row gap-8 m-0 p-5 list-none ">
+          {navItems.map((item, idx) => (
+            <li
+              key={item.href}
+              className={`px-0 py-1 border-t-2 border-b-2 ${itemColors[idx]}`}
+            >
+              <a
+                href={item.href}
+                className={`block text-left font-medium transition-colors duration-500 ${textHoverColors[idx]}`}
+              >
+                {item.name}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
     </nav>
   );
 };
