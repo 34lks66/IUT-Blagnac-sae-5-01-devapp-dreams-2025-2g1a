58e1946b258915f531bf3ecfeb54e04847b9ecca
34lks66 <uranus667788@gmail.com>
2025-09-20 16:34:43 +0200

    refactoring

--- dev/site_dreams/src/App.tsx
+++ dev/site_dreams/src/App.tsx
@@ -1,8 +1,8 @@
-import './App.css'
-import Header from './components/Header'
-import MenuPays from './components/Menu_pays'
-import FooterMain from "./components/Footer";
-import NavBar from './components/NavBar'
+import './App.css';
+import Header from './components/Header';
+import MenuPays from './components/Menu_pays';
+import FooterMain from './components/Footer';
+import NavBar from './components/NavBar';
 
 function App() {
 
