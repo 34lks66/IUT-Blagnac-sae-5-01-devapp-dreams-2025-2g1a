60b62a0332813f97a95b8355b45390567c6bc3f7
lphrakousonh <loiclaloix@gmail.com>
2025-10-01 14:08:52 +0200

    ajout section page accompagnement

--- dev/site_dreams/src/pages/accompagnement.tsx
+++ dev/site_dreams/src/pages/accompagnement.tsx
@@ -1,6 +1,6 @@
 import doclogo from "../assets/documentlogo.svg";
 import suivilogo from "../assets/suivilogo.svg";
-
+import justicelogo from "../assets/justicelogo.svg";
 function Accompagnement() {
     return (
         <main className="max-w-5xl mx-auto px-4">
@@ -21,10 +21,37 @@ function Accompagnement() {
 
             {/* Section Que proposons-nous ? */}
             <section className="mb-1">
-                <div className="grid md:grid-cols-1 gap-6">
+                <div className="grid md:grid-cols-1 gap-15">
 
                     {/* Carte 1 */}
-                    <div className="relative bg-white p-6 rounded-xl shadow-lg hover:shadow-xl transition-all duration-300 transform hover:-translate-y-1 border-l-4 border-b-4 border-red-500 overflow-hidden flex flex-col md:flex-row items-center">
+                    <div className="group relative bg-white p-6 rounded-xl shadow-lg hover:shadow-xl transition-all duration-500 transform hover:-translate-y-1 border-l-4 border-b-4 border-red-500 overflow-hidden flex flex-col md:flex-row items-center mr-32 cursor-pointer">
+
+                        <div className="absolute inset-0 bg-red-500 transform -translate-x-full group-hover:translate-x-0 transition-transform duration-500 ease-[cubic-bezier(0.65,0,0.35,1)] z-0"></div>
+
+                        {/* Contenu texte */}
+                        <div className="relative z-10 flex-1 transition-all duration-300 group-hover:text-white">
+                            <h3 className="text-xl font-bold mb-3 text-gray-800 group-hover:text-white transition-colors duration-300">
+                                Orientation dans les démarches
+                            </h3>
+                            <p className="text-gray-600 mb-4 leading-relaxed text-sm group-hover:text-white transition-colors duration-300">
+                                Aide pour remplir les formulaires et comprendre les procédures
+                                (asile, préfecture, CAF…).
+                            </p>
+                        </div>
+
+
+                        <div className="relative z-10 flex items-center justify-center flex-1">
+                            <img
+                                src={doclogo}
+                                alt="Icône de documents"
+                                className="w-32 h-32 md:w-44 md:h-44 transition-all duration-500 group-hover:translate-x-20 group-hover:opacity-0 group-hover:scale-75"
+                            />
+                        </div>
+
+                    </div>
+
+                    {/* Carte 1 */}
+                    <div className="relative bg-white p-6 rounded-xl shadow-lg hover:shadow-xl transition-all duration-300 transform hover:-translate-y-1 border-l-4 border-b-4 border-red-500 overflow-hidden flex flex-col md:flex-row items-center mr-32">
                         <div className="relative z-10 flex-1">
                             <h3 className="text-xl font-bold mb-3 text-gray-800">Orientation dans les démarches</h3>
                             <p className="text-gray-600 mb-4 leading-relaxed text-sm">
@@ -39,39 +66,29 @@ function Accompagnement() {
                     </div>
 
 
-
-
-
-
-                    {/* Carte 1 */}
-                    {/* <div className="bg-white p-6 shadow-lg rounded-xl hover:shadow-xl transition">
-                        <h3 className="text-xl font-semibold mb-2 text-red-500">
-                            Orientation dans les démarches
-                        </h3>
-                        <p className="text-gray-600">
-                            Aide pour remplir les formulaires et comprendre les procédures
-                            (asile, préfecture, CAF…).
-                        </p>
-                    </div> */}
-
                     {/* Carte 2 */}
-                    <div className="bg-white p-6 shadow-lg rounded-xl hover:shadow-xl transition">
-                        <h3 className="text-xl font-semibold mb-2 text-red-500">
-                            Accompagnement juridique
-                        </h3>
-                        <p className="text-gray-600">
-                            Informations sur les droits et aide à préparer des dossiers
-                            (asile, régularisation, logement, emploi).
-                        </p>
+                    <div className="relative bg-white p-6 rounded-xl shadow-lg hover:shadow-xl transition-all duration-300 transform hover:-translate-y-1 border-r-4 border-b-4 border-purple-500 overflow-hidden flex flex-col md:flex-row items-center  ml-32">
+                        {/* Image décorative */}
+                        <div className="flex items-center justify-center flex-1">
+                            <img src={justicelogo} alt="Icône juridique" className="w-32 h-32 md:w-44 md:h-44" />
+                        </div>
+                        <div className="relative z-10 flex-1">
+                            <h3 className="text-xl font-bold mb-3 text-gray-800">Accompagnement juridique</h3>
+                            <p className="text-gray-600 mb-4 leading-relaxed text-sm">
+                                Informations sur les droits et aide à préparer des dossiers
+                                (asile, régularisation, logement, emploi).
+                            </p>
+                        </div>
                     </div>
 
+
                     {/* Carte 3 */}
-                    <div className="relative bg-white p-6 rounded-xl shadow-lg hover:shadow-xl transition-all duration-300 transform hover:-translate-y-1 border-l-4 border-b-4 border-blue-500 overflow-hidden flex flex-col md:flex-row items-center">
+                    <div className="relative bg-white p-6 rounded-xl shadow-lg hover:shadow-xl transition-all duration-300 transform hover:-translate-y-1 border-l-4 border-b-4 border-blue-500 overflow-hidden flex flex-col md:flex-row items-center  mr-32">
                         <div className="relative z-10 flex-1">
                             <h3 className="text-xl font-bold mb-3 text-gray-800">Suivi personnalisé</h3>
                             <p className="text-gray-600 mb-4 leading-relaxed text-sm">
                                 Accompagnement au cas par cas : suivi régulier selon les besoins
-                            spécifiques de chaque personne.
+                                spécifiques de chaque personne.
                             </p>
                         </div>
                         {/* Image décorative */}
@@ -80,26 +97,36 @@ function Accompagnement() {
                         </div>
                     </div>
 
-                    
+
                 </div>
             </section>
 
             {/* Section Redirection vers formulaire */}
             <section className="text-center">
-                <h2 className="text-2xl md:text-3xl font-bold text-gray-800 mb-4">
-                    Besoin d’aide ?
-                </h2>
-                <p className="text-gray-600 mb-6">
-                    Remplissez notre formulaire d’accompagnement et un bénévole DREAMS
-                    prendra contact avec vous rapidement.
-                </p>
-                <a
-                    href="/formulaire-aide"
-                    className="inline-block px-6 py-3 rounded-lg bg-red-600 text-white font-semibold hover:bg-red-700 transition"
+                <button
+                    className="group relative item card rounded-xl text-center border-t-4 border-t-red-500 bg-white shadow-md px-3 md:px-5 xl:px-40 2xl:px-48 py-3 mb-2 overflow-hidden transition-all duration-400 hover:shadow-lg"
+                    onClick={() => {
+                        window.location.href = "/formulaire-aide";
+                    }}
                 >
-                    Remplir le formulaire
-                </a>
+                    <div className="absolute inset-0 bg-red-500 transform -translate-x-full group-hover:translate-x-0 transition-transform duration-500 ease-[cubic-bezier(0.65,0,0.35,1)] z-0"></div>
+                    <div className="relative z-10">
+                        <div className="title-content flex justify-start md:justify-center">
+                            <span className="font-bold title font-title text-gray-800 group-hover:text-white transition-colors duration-300">
+                                Besoin d’aide ?
+                            </span>
+                        </div>
+                        <div className="btn-content flex justify-center md:justify-center mb-3 mt-3">
+                            <span className="btn my-3 text-gray-600 group-hover:text-white transition-colors duration-300">
+                                Remplissez notre formulaire d’accompagnement et un bénévole <span className="font-bold">DREAMS </span>
+                                prendra contact avec vous rapidement.
+                            </span>
+                        </div>
+                    </div>
+                </button>
             </section>
+
+            
 
 
         </main>
