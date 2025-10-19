6d4083b22f67fe79cd8e0a593a8e8535e483a5f7
34lks66 <uranus667788@gmail.com>
2025-10-19 20:03:54 +0200

    creation api doc

--- dev/backend/Server.js
+++ dev/backend/Server.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const swaggerUi = require('swagger-ui-express');
+const swaggerSpec = require('./docs/swagger');
+const fs = require('fs');
+const path = require('path');
 
 const routes = require('./routes/MemberRoute');
 
@@ -29,6 +33,67 @@ app.get('/', (req, res) => {
     });
 });
 
+app.get('/swagger.json', (req, res) => {
+  res.setHeader('Content-Type', 'application/json');
+  res.send(swaggerSpec);
+});
+
+ app.get('/api-docs-redoc', (req, res) => {
+   res.send(`
+     <!DOCTYPE html>
+     <html>
+       <head>
+         <title>API Documentation - ReDoc</title>
+         <meta charset="utf-8"/>
+         <style>
+           body { margin: 0; padding: 0; background-color: #0f0f0f; }
+        .menu-content {
+            border-right: 1px solid #a0a0a0 !important;
+        }
+         </style>
+       </head>
+       <body>
+        <div id="redoc-container"></div>
+         <script src="https://cdn.redoc.ly/redoc/latest/bundles/redoc.standalone.js"></script>
+         <script>
+          Redoc.init('/swagger.json', {
+            theme: {
+              colors: {
+                text: {
+                  primary: '#ffffff',
+                  secondary: '#d1d5db' 
+                },
+                background: {
+                  main: '#0f0f0f',
+                  paper: '#1a1a1a'
+                }
+              },
+              sidebar: {
+                backgroundColor: '#0f0f0f',
+                textColor: '#ffffff',
+                width: '260px',
+                hover: {
+                  backgroundColor: '#d6d9da'
+                }
+              },
+              rightPanel: {
+                backgroundColor: '#0f0f0f'
+              }
+            },
+            pathInMiddlePanel: true,
+            hideDownloadButton: true,
+            expandSingleSchemaField: true
+          }, document.getElementById('redoc-container'));
+        </script>
+       </body>
+     </html>
+   `);
+ });
+
 app.use('/api', routes);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Docs: http://localhost:${PORT}/api-docs`));
+app.listen(PORT, () => console.log(`ReDoc: http://localhost:${PORT}/api-docs-redoc`));
