eca0b649fc5dd2cf0961e05fbb3e4a088dd6e4f5
lphrakousonh <loiclaloix@gmail.com>
2025-10-20 12:43:10 +0200

    ajout du login dans le dashboard

---
+++ dev/dashboard/src/App.tsx
@@ -0,0 +1,20 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Dashboard from "./Dashboard";
+import Login from "./components/login";
+
+function App() {
+
+    return (
+        <>
+            <Router>
+                <Routes>
+                    <Route path="/" element={<Dashboard />} />
+                    <Route path="/login" element={<Login />} />
+                </Routes>
+            </Router>
+        </>
+
+    )
+}
+
+export default App
