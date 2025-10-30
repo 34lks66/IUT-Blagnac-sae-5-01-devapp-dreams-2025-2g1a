6ca29ce16e04e246d00f764a7845e1a5628eb53d
Ezeloss <leo.guinvarch@gmail.com>
2025-10-30 20:58:15 +0100

    fixing + modif structure de données event

--- dev/backend/models/EventModel.js
+++ dev/backend/models/EventModel.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 const EventSchema = new mongoose.Schema({
   title: { type: String, required: true },
   date: { type: String, required: true },
-  time: { type: String },
+  starttime: { type: String },
+  endtime: { type: String },
   location: { type: String },
   description: { type: String },
   antenna: { type: mongoose.Schema.Types.ObjectId, ref: 'Antenne', default: null },
