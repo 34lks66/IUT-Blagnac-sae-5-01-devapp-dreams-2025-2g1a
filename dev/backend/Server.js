45b60cc60df3a0ad85338fc7f77f5d34f1079e0b
34lks66 <uranus667788@gmail.com>
2025-11-17 17:20:59 +0100

    fix bug

--- dev/backend/Server.js
+++ dev/backend/Server.js
@@ -25,7 +25,8 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:5174", "https://6qbmdkkp-5174.uks1.devtunnels.ms"], 
+    // origin: ["http://localhost:5173", "http://localhost:5174", "https://6qbmdkkp-5174.uks1.devtunnels.ms"],
+    origin: ["http://localhost:5173", "http://localhost:5174"], 
     credentials: true, 
   })
 );
