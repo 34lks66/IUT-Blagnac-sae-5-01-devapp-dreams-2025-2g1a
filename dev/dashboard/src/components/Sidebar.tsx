e2daac5a2a6db26812ac2c101a88f7c55784436a
lphrakousonh <loiclaloix@gmail.com>
2026-01-13 00:43:59 +0100

    reglage bug reload infini

--- dev/dashboard/src/components/Sidebar.tsx
+++ dev/dashboard/src/components/Sidebar.tsx
@@ -31,25 +31,7 @@ const Sidebar = ({
 }: SidebarProps) => {
   const { role } = useAuth();
   const menuItems = [
-    {
-      id: "beneficiaires",
-      label: "Bénéficiaires",
-      icon: (
-        <svg
-          className="w-5 h-5"
-          fill="none"
-          stroke="currentColor"
-          viewBox="0 0 24 24"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth={2}
-            d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z"
-          />
-        </svg>
-      ),
-    },
+
     {
       id: "settings",
       label: "Paramètres",
@@ -79,8 +61,27 @@ const Sidebar = ({
 
   if (role !== "O") {
     menuItems.splice(
-      1,
       0,
+      0,
+      {
+        id: "beneficiaires",
+        label: "Bénéficiaires",
+        icon: (
+          <svg
+            className="w-5 h-5"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z"
+            />
+          </svg>
+        ),
+      },
       {
         id: "dashboard",
         label: "Tableau de bord",
@@ -296,10 +297,9 @@ const Sidebar = ({
                     className={`
                       w-full text-left px-4 py-3 rounded-lg transition-all duration-200
                       flex items-center gap-3 font-medium text-sm
-                      ${
-                        activeTab === item.id
-                          ? "bg-white text-[#93720a] shadow-lg"
-                          : "hover:bg-white/10 text-black/90"
+                      ${activeTab === item.id
+                        ? "bg-white text-[#93720a] shadow-lg"
+                        : "hover:bg-white/10 text-black/90"
                       }
                     `}
                   >
